48c8940d4b124fcb61109f41bb63fbe0ed12a01a
siyad7siyad <muhammedsiyad17127@gmail.com>
2024-08-22 00:24:16 +0530

    Removed resetPasswordRequest imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
-import ResetPasswordRequest from "./pages/resetPasswordRequest";
-import ResetPassword from "./pages/resetPassword";
-
 
 function App() {
   return (
@@ -29,8 +26,6 @@ function App() {
             </Layout>
           }
         />
-         <Route path="/reset-password-request" element={<Layout><ResetPasswordRequest /></Layout>} />
-         <Route path="/reset-password/:token" element={<Layout><ResetPassword /></Layout>} />
       </Routes>
     </BrowserRouter>
   );
